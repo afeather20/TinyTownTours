a3824b64873e079ac07b5d0b7d808a540d0634b0
Adam Feather <afeather@thinklogic.net>
2020-04-10 14:19:27 -0700

    added location

--- tourapp/public/javascripts/index.js
+++ tourapp/public/javascripts/index.js
@@ -1,14 +1,25 @@
 $(document).ready(function() { 
-
+  var userPosition = []; 
+  var map;
+   navigator.geolocation.getCurrentPosition(showPosition)
     mapboxgl.accessToken = 'pk.eyJ1IjoiYWZlYXRoZXIyMCIsImEiOiJjazhmaDlrYmEwNDg2M2dzMHRycG4wMXJzIn0._FYX6dOkYeSWZTCyQtZs0w';
     // var map = L.mapbox.map('map')
-    // .setView([47.6249, -122.5210], 12)
+
     // .addLayer(L.mapbox.styleLayer(PageData.mapStyles.styleurl));
 
-    var map = new mapboxgl.Map({
+    function showPosition(position) {
+
+      var positionArray = [position.coords.latitude,position.coords.longitude ]
+      userPosition.push(position.coords.longitude); 
+      userPosition.push(position.coords.latitude);
+      console.log(userPosition);
+      map = new mapboxgl.Map({
         container: 'map', // container id
         style: PageData.mapStyles.styleurl, // stylesheet location
-        center: [-122.5210,47.6249 ], // starting position
+        center: userPosition, // starting position
         zoom: 12 // starting zoom
       });
+    }
+   
+    
 });
