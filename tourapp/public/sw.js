e3d54120aa9f6fb4740090f81487acc389a2004b
Adam Feather <afeather@thinklogic.net>
2020-04-23 21:40:45 -0400

    how about this

--- tourapp/public/sw.js
+++ tourapp/public/sw.js
@@ -19,10 +19,10 @@ self.addEventListener('fetch', event => {
     const req = event.request;
     const url = new URL(req.url);
     // if (url.origin === location.url) {
-    //     event.respondWith(cacheFirst(req));
+        event.respondWith(cacheFirst(req));
     // } else {
-        event.respondWith(networkFirst(req));
-    //}
+        // event.respondWith(networkFirst(req));
+    // }
 });
 
 async function cacheFirst(req) {
